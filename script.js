869c6ca591a2ef742069df992bea26d95cd2db26
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-06-26 16:12:45 +0900

    Initial commit

--- script.js
+++ script.js
@@ -47,12 +47,14 @@
 
     function mup(e) {
         var drag = document.getElementsByClassName("drag")[0];
-
-        document.body.removeEventListener("mousemove", mmove, false);
-        drag.removeEventListener("mouseup", mup, false);
-        document.body.removeEventListener("touchmove", mmove, false);
-        drag.removeEventListener("touchend", mup, false);
-
-        drag.classList.remove("drag");
+    
+        if (drag) {
+            document.body.removeEventListener("mousemove", mmove, false);
+            drag.removeEventListener("mouseup", mup, false);
+            document.body.removeEventListener("touchmove", mmove, false);
+            drag.removeEventListener("touchend", mup, false);
+    
+            drag.classList.remove("drag");
+        }
     }
 })();
