c04b3cfb31df488128780477c60faf732d466bc9
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-06-27 19:34:09 +0900

    Initial commit

--- script.js
+++ script.js
@@ -1,98 +1,67 @@
-(function(){
-
+(function() {
     //要素の取得
-    var elements = document.getElementsByClassName("container");
+    var container = document.getElementById('container');
 
     //要素内のクリックされた位置を取得するグローバル（のような）変数
     var x;
     var y;
 
-    //マウスが要素内で押されたとき、又はタッチされたとき発火
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("mousedown", mdown, false);
-        elements[i].addEventListener("touchstart", mdown, false);
+    //ドラッグアンドドロップのイベント設定
+    function setDragAndDrop(element) {
+        element.addEventListener("mousedown", mdown, false);
+        element.addEventListener("touchstart", mdown, false);
     }
 
-        //四角形の複製
-        container.addEventListener('contextmenu', function(event) {
-            event.preventDefault();
-            if (event.target.classList.contains('square')) {
-                var newSquare = event.target.cloneNode(true);
-                newSquare.style.top = `${event.clientY - container.offsetTop}px`;
-                newSquare.style.left = `${event.clientX - container.offsetLeft}px`;
-                container.appendChild(newSquare);
-                setDragAndDrop(newSquare);
-            }
-        });
+    //四角形の複製
+    container.addEventListener('contextmenu', function(event) {
+        event.preventDefault();
+        if (event.target.classList.contains('square')) {
+            var newSquare = event.target.cloneNode(true);
+            newSquare.style.top = `${event.clientY - container.offsetTop}px`;
+            newSquare.style.left = `${event.clientX - container.offsetLeft}px`;
+            container.appendChild(newSquare);
+            setDragAndDrop(newSquare);
+        }
+    });
 
     //マウスが押された際の関数
     function mdown(e) {
-
-        //クラス名に .drag を追加
+        var event = e.type === "mousedown" ? e : e.changedTouches[0];
         this.classList.add("drag");
-
-        //タッチデイベントとマウスのイベントの差異を吸収
-        if(e.type === "mousedown") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
-
-        //要素内の相対座標を取得
         x = event.pageX - this.offsetLeft;
         y = event.pageY - this.offsetTop;
 
-        //ムーブイベントにコールバック
         document.body.addEventListener("mousemove", mmove, false);
         document.body.addEventListener("touchmove", mmove, false);
     }
 
     //マウスカーソルが動いたときに発火
     function mmove(e) {
-
-        //ドラッグしている要素を取得
         var drag = document.getElementsByClassName("drag")[0];
-
-        //同様にマウスとタッチの差異を吸収
-        if(e.type === "mousemove") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
-
-        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+        var event = e.type === "mousemove" ? e : e.changedTouches[0];
         e.preventDefault();
-
-        //マウスが動いた場所に要素を動かす
         drag.style.top = event.pageY - y + "px";
         drag.style.left = event.pageX - x + "px";
 
-        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
         drag.addEventListener("mouseup", mup, false);
         document.body.addEventListener("mouseleave", mup, false);
         drag.addEventListener("touchend", mup, false);
         document.body.addEventListener("touchleave", mup, false);
-
     }
 
     //マウスボタンが上がったら発火
     function mup(e) {
         var drag = document.getElementsByClassName("drag")[0];
-
-        //ムーブベントハンドラの消去
         document.body.removeEventListener("mousemove", mmove, false);
         drag.removeEventListener("mouseup", mup, false);
         document.body.removeEventListener("touchmove", mmove, false);
         drag.removeEventListener("touchend", mup, false);
-
-        //クラス名 .drag も消す
         drag.classList.remove("drag");
     }
 
-        // 初期四角形の設定
-        var elements = document.getElementsByClassName("drag-and-drop");
-        for (var i = 0; i < elements.length; i++) {
-            setDragAndDrop(elements[i]);
-        }
-
-})()
+    // 初期四角形の設定
+    var elements = document.getElementsByClassName("drag-and-drop");
+    for (var i = 0; i < elements.length; i++) {
+        setDragAndDrop(elements[i]);
+    }
+})();
