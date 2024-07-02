eb6759d82ed714c6f154a5e77e424c98e4feef7c
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-07-02 14:39:28 +0900

    Initial commit

--- script.js
+++ script.js
@@ -12,14 +12,31 @@
         element.addEventListener("touchstart", mdown, false);
     }
 
+    setDragAndDrop(square);
+
     //四角形の複製
+    //container要素にcontextmenuイベントリスナーを追加
     container.addEventListener('contextmenu', function(event) {
+
+        //デフォルトのコンテキストメニュー（右クリックメニュー）が表示されるのを防ぐ
         event.preventDefault();
+
+        //右クリックされた要素がclass="square"を持っているかどうかを確認
         if (event.target.classList.contains('square')) {
+
+            //右クリックされた四角形を複製。cloneNode(true)は、その要素とその子要素もすべて複製
             var newSquare = event.target.cloneNode(true);
+
+            //新しい四角形の位置を設定
+            //event.clientYとevent.clientXは、右クリックされた位置のY座標とX座標
+            //container.offsetTopとcontainer.offsetLeftは、コンテナ要素の上端と左端からのオフセット
             newSquare.style.top = `${event.clientY - container.offsetTop}px`;
             newSquare.style.left = `${event.clientX - container.offsetLeft}px`;
+
+            //新しい四角形をcontainer要素に追加
             container.appendChild(newSquare);
+
+            //新しい四角形にドラッグアンドドロップ機能を設定する関数setDragAndDropを呼び出す
             setDragAndDrop(newSquare);
         }
     });
@@ -69,9 +86,4 @@
         drag.classList.remove("drag");
     }
 
-    // 初期四角形の設定
-//    var elements = document.getElementsByClassName("drag-and-drop");
-//    for (var i = 0; i < elements.length; i++) {
-//        setDragAndDrop(elements[i]);
-//    }
 })();
